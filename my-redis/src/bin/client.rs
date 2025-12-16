59201a415770ad8b701b082e8361b0860ded04f0
hsuehly <you@example.com>
2025-12-16 21:21:44 +0800

    update

---
+++ my-redis/src/bin/client.rs
@@ -0,0 +1,79 @@
+use bytes::Bytes;
+use mini_redis::client;
+use tokio::sync::{mpsc, oneshot};
+
+#[derive(Debug)]
+enum Command {
+    Get {
+        key: String,
+        resp: Responder<Option<Bytes>>,
+    },
+    Set {
+        key: String,
+        val: Bytes,
+        resp: Responder<()>,
+    },
+}
+type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
+#[tokio::main]
+async fn main() {
+    let (tx, mut rx) = mpsc::channel(32);
+    let tx2 = tx.clone();
+    let manager = tokio::spawn(async move {
+        let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+        while let Some(cmd) = rx.recv().await {
+            match cmd {
+                Command::Get { key, resp } => {
+                    let res = client.get(&key).await;
+                    let _ = resp.send(res);
+                }
+                Command::Set { key, val, resp } => {
+                    let res = client.set(&key, val).await;
+                    // Ignore errors
+                    let _ = resp.send(res);
+                }
+            }
+        }
+    });
+    // 创建一个消息通道 缓冲是 32
+
+    let t1 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
+        let cmd = Command::Get {
+            key: "hello".to_string(),
+            resp: resp_tx,
+        };
+
+        tx.send(cmd).await.unwrap();
+        let res = resp_rx.await;
+        println!("GOT = {:?}", res);
+    });
+
+    let t2 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
+        let cmd = Command::Set {
+            key: "foo".to_string(),
+            val: "bar".into(),
+            resp: resp_tx,
+        };
+
+        tx2.send(cmd).await.unwrap();
+        // 等待回复
+        let res = resp_rx.await;
+        println!("GOT = {:?}", res);
+    });
+    t1.await.unwrap();
+    t2.await.unwrap();
+    manager.await.unwrap();
+
+    // let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+    // // 生成两个任务，一个用于获取 key, 一个用于设置 key
+    // let t1 = tokio::spawn(async {
+    //     let res = client.get("hello").await;
+    // });
+    // let t2 = tokio::spawn(async {
+    //     client.set("foo", "bar".into()).await;
+    // });
+    // t1.await.unwrap();
+    // t2.await.unwrap();
+}
